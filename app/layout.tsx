aa6e329ca932b4c280c6db1ab3ae654cb3575000
nvhunggg <hungnv2@runsystem.net~>
2025-03-03 17:08:19 +0700

    610

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Navbar from '@/components/navbar/Navbar';
 import Container from '@/components/global/Container';
 import Providers from './providers';
+import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,16 +19,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang='en' suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
-        <Navbar />
-        <Container className='py-20'>
-          {children}
-          </Container>
+          <Navbar />
+          <Container className='py-20'>{children}</Container>
         </Providers>
-        
-        </body>
+      </body>
     </html>
+  </ClerkProvider>
   );
 }
